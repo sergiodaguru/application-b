d25f6eb098037e483af504d79ffd8178b3e85c75
sergiod <sergiod@us.ibm.com>
2023-06-19 15:49:01 -0400

    Change msg

--- routes/messages.js
+++ routes/messages.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
-let text;
-let counter = 0;
+
+let ran = 0;
+function f() {
+   ran++;
+   console.log("Test ABC Message " + ran)
+   setTimeout(f, 0);
+}
 
 /* GET greeting */
 router.get('/messages', function(req, res, next) {
-    let i
-     for (i = 0; i < 50; i++) {
-        text += "<br>The number is " + i + "<br>";
-        console.log(text);
-     }
-     res.send("<p>Sent 50 log Messages.</p>");
+  f();
+
+  setTimeout(() => { res.send(
+                         { "Messages": ran }
+                       ) }, 5000);
 });
 
 module.exports = router;
